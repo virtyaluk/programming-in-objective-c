5af8e3057e74f0fe829b0e2bd22df640a652fc19
virtyaluk <winsatana@ya.ru>
2016-12-06 16:27:15 +0200

    Adds exercise 4

---
+++ src/chapter2/exercises/exercise4.m
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char *argv[])
+{
+    @autoreleasepool {
+        int res = 87 - 15;
+
+        NSLog(@"Result of substraction 15 from 87 is %i", res);
+    }
+
+    return 0;
+}
