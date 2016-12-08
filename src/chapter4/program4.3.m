3d2c17fbbb41fa6547e60780c1cd419040376a8f
virtyaluk <winsatana@ya.ru>
2016-12-08 16:58:50 +0200

    Adds program4.3

---
+++ src/chapter4/program4.3.m
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char *argv[])
+{
+    @autoreleasepool {
+        int a = 25, b = 2;
+        float c = 25.0, d = 2.0;
+
+        NSLog(@"6 + a / 5 * b = %i", 6 + a/ 5 * b);
+        NSLog(@"a / b * b = %i", a / b * b);
+        NSLog(@"c / d * d = %f", c / d * d);
+        NSLog(@"-a = %i", -a);
+    }
+
+    return 0;
+}
