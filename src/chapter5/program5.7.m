51c221aa96bb9736bd6849fdea0e5455c463f670
virtyaluk <winsatana@ya.ru>
2016-12-11 01:57:48 +0200

    Adds program5.7

---
+++ src/chapter5/program5.7.m
@@ -0,0 +1,22 @@
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char *argv[])
+{
+    @autoreleasepool {
+        unsigned int u, v, temp;
+
+        NSLog(@"Please type in two nonnegative integers.");
+        scanf("%u%u", &u, &v);
+
+        while (v != 0)
+        {
+            temp = u % v;
+            u = v;
+            v = temp;
+        }
+
+        NSLog(@"Their greatest common divisor is %u", u);
+    }
+
+    return 0;
+}
