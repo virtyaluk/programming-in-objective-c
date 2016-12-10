8fd639bd9033ccbeac3e8fdfdd73cb4ce203cf71
virtyaluk <winsatana@ya.ru>
2016-12-11 01:54:33 +0200

    Adds program5.6

---
+++ src/chapter5/program5.6.m
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char *argv[])
+{
+    @autoreleasepool {
+        int count = 1;
+
+        while (count <= 5)
+        {
+            NSLog(@"%i", count);
+            count++;
+        }
+    }
+
+    return 0;
+}
